2faa7fa46d35e6c6183c7201f12f6df7c758ad1e
Sean Feldman <feldman.sean@gmail.com>
2015-10-11 13:04:23 -0600

    Part 3 - failing to dispatch a message (failing to send a command)

--- LostNotification/Marketing/INewUserRegisteredHandler.cs
+++ LostNotification/Marketing/INewUserRegisteredHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Messages.Registration.Commands;
 using Messages.Registration.Events;
 using NServiceBus;
 
@@ -6,9 +7,18 @@ namespace Marketing
 {
     public class INewUserRegisteredHandler : IHandleMessages<INewUserRegistered>
     {
+        public IBus Bus { get; set; }
+
         public void Handle(INewUserRegistered message)
         {
             Console.WriteLine($"Received notification that '{message.Email}' got registered (registration date: {message.RegistrationDateUtc.ToShortDateString()})");
+            Console.WriteLine("Sending marketing stuff...");
+            Bus.Send<SendNotification>(m =>
+            {
+                m.Email = message.Email;
+                m.Title = "50% off!";
+                m.Content = "As a new registrant you're entitled for 50% off out entire stock. Don't miss this opportunity.";
+            });
         }
     }
 }
